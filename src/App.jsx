36e4a40b284244a117050b073bd5b104b85a0c56
lien <lien3002@hotmail.com>
2023-10-18 18:40:44 -0500

    implementacion-de-los-clics-reiciniarClics

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import logo1 from "./imagenes/logo1.jpg";
 import  Boton  from "./componetes/boton.jsx";
 import './hojas-de-estilo/Botones.css';
 import Contador from "./componetes/contador.jsx";
+import "./hojas-de-estilo/contador.css"
 
 function App() {
   const [count, setCount] = useState(0);
@@ -19,12 +20,12 @@ function App() {
         <img className="freelogo" src={logo1} alt="Logo de free code" />
       </div>
       <div className="contenedorPrincipal">
+        <Contador numClics={5}></Contador>
         <Boton
           texto='Clic'
           esBotonClic={true} 
           manejarClic={manejarClic}>
         </Boton>
-        <Contador numClics={5}></Contador>
         <Boton
           texto='Reiniciar' 
           esBotonClic={false} 
