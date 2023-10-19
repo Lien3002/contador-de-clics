4f4b1374f476c296863daeb2ca6f1f11a3fc8b9e
lien <lien3002@hotmail.com>
2023-10-18 19:20:05 -0500

    Final-ordenando

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import { useState } from "react";
 import "./App.css";
 import logo1 from "./imagenes/logo1.jpg";
-import  Boton  from "./componetes/boton.jsx";
-import './hojas-de-estilo/Botones.css';
+import Boton from "./componetes/boton.jsx";
+import "./hojas-de-estilo/Botones.css";
 import Contador from "./componetes/contador.jsx";
-import "./hojas-de-estilo/contador.css"
+import "./hojas-de-estilo/contador.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
   const manejarClic = () => {
-    setCount(count+1);
+    setCount(count + 1);
   };
   const reiniciarClic = () => {
     setCount(0);
@@ -23,15 +23,15 @@ function App() {
       <div className="contenedorPrincipal">
         <Contador numClics={count}></Contador>
         <Boton
-          texto='Clic'
-          esBotonClic={true} 
-          manejarClic={manejarClic}>
-        </Boton>
+          texto="Clic"
+          esBotonClic={true}
+          manejarClic={manejarClic}
+        ></Boton>
         <Boton
-          texto='Reiniciar' 
-          esBotonClic={false} 
-          manejarClic={reiniciarClic}>
-        </Boton>
+          texto="Reiniciar"
+          esBotonClic={false}
+          manejarClic={reiniciarClic}
+        ></Boton>
       </div>
     </div>
   );
--- src/componetes/contador.jsx
+++ src/componetes/contador.jsx
@@ -1,10 +1,6 @@
 import React from "react";
 
-function Contador({numClics}) {
-  return(
-    <div className="contador">
-      {numClics}
-    </div>
-  )
+function Contador({ numClics }) {
+  return <div className="contador">{numClics}</div>;
 }
 export default Contador;
